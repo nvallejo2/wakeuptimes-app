d678a41333e025667922fe901cffd1dd48c0a4b9
nvallejo2 <nvallej1@jhu.edu>
2024-09-01 19:15:20 -0400

    Finished Task 5

--- wakeuptimes-app/index.js
+++ wakeuptimes-app/index.js
@@ -25,30 +25,46 @@ for (let i = 1; i <= 6; i++) {
 console.log("Wake-up times:", wakeUpTimes.join(", "));
 */
 
-const option = {timeStyle: "short"};
-
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
 const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
+
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+  promptSection.classList.remove("hidden");
+  imageContainer.classList.remove("hidden");
+  resultSection.classList.add("hidden");
+});
+
+const option = { timeStyle: "short" };
 
 function calcWakeUpTimes() {
-    const fallAsleepTime = new Date();
-    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-   
-    const wakeUpTime = new Date(fallAsleepTime);
-    wakeUpHoursDiv.innerHTML = "";
+  const fallAsleepTime = new Date();
+  fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
-    for (let i = 1; i <= 6; i++) {
-      wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
-      const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", option);
-      //const cycleDiv = document.getElementById(`cycle-${i}`);
-      //cycleDiv.textContent = wakeUpTimeString;
-      const cycleDiv = document.createElement("div");
-      cycleDiv.classList.add("cycle");
-      cycleDiv.setAttribute("id", `cycle-${i}`);
-      cycleDiv.textContent = wakeUpTimeString;
-      wakeUpHoursDiv.appendChild(cycleDiv);
-    }
+  const wakeUpTime = new Date(fallAsleepTime);
+  wakeUpHoursDiv.innerHTML = "";
 
+  for (let i = 1; i <= 6; i++) {
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", option);
+    //const cycleDiv = document.getElementById(`cycle-${i}`);
+    //cycleDiv.textContent = wakeUpTimeString;
+    const cycleDiv = document.createElement("div");
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
   }
-   
+  promptSection.classList.add("hidden");
+  imageContainer.classList.add("hidden");
+  resultSection.classList.remove("hidden");
+}
+
 const calcBtn = document.getElementById("calc-btn");
 calcBtn.onclick = calcWakeUpTimes;
