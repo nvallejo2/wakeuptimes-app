e503775316c8d7830fdf9f5550d91f87248e38ee
nvallejo2 <nvallej1@jhu.edu>
2024-09-01 18:08:23 -0400

    Finished Task 3 and 4

--- wakeuptimes-app/index.js
+++ wakeuptimes-app/index.js
@@ -1,4 +1,4 @@
-const now = new Date();
+/*const now = new Date();
 //what is the time right now?
 console.log(now.toLocaleTimeString("en-US"));
 
@@ -23,3 +23,32 @@ for (let i = 1; i <= 6; i++) {
     wakeUpTimes.push(wakeUpTimeString);
 }
 console.log("Wake-up times:", wakeUpTimes.join(", "));
+*/
+
+const option = {timeStyle: "short"};
+
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+
+function calcWakeUpTimes() {
+    const fallAsleepTime = new Date();
+    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+   
+    const wakeUpTime = new Date(fallAsleepTime);
+    wakeUpHoursDiv.innerHTML = "";
+
+    for (let i = 1; i <= 6; i++) {
+      wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+      const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", option);
+      //const cycleDiv = document.getElementById(`cycle-${i}`);
+      //cycleDiv.textContent = wakeUpTimeString;
+      const cycleDiv = document.createElement("div");
+      cycleDiv.classList.add("cycle");
+      cycleDiv.setAttribute("id", `cycle-${i}`);
+      cycleDiv.textContent = wakeUpTimeString;
+      wakeUpHoursDiv.appendChild(cycleDiv);
+    }
+
+  }
+   
+const calcBtn = document.getElementById("calc-btn");
+calcBtn.onclick = calcWakeUpTimes;
